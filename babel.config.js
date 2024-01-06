44aa6c08258e3a6de214e9918b68388191eb8b2d
yamisora-sys <20520930@ms.uit.edu.vn>
2024-01-06 17:42:28 +0700

    Update dependencies and add module resolver configuration

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,27 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-classname-to-style',
-      ['react-native-platform-specific-extensions', {extensions: ['css']}]
+      ['react-native-platform-specific-extensions', {extensions: ['css']}],
+      ['module-resolver', {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@assets': './assets',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@config': './src/config',
+          '@context': './src/context',
+          '@models': './src/models',
+          '@types': './src/types',
+          '@theme': './src/theme',
+          '@pages': './src/pages',
+        },
+      }]
     ]
   };
 };
